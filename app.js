e90734006ac184a934d89b63dfef9c999ff7d66a
ahmetoztrk <ahmetoztrk1709@gmail.com>
2023-04-16 17:03:42 +0300

    added product schema

--- app.js
+++ app.js
@@ -1,28 +1,54 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Product = require('./models/Product');
 
 require('dotenv/config');
 const api = process.env.API_URL;
 
 const app = express();
 
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then(() => {
+    console.log('Database connection is ready..');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 //Middlewares
 app.use(express.json());
 app.use(morgan('tiny'));
 
-app.get(`${api}/products`, (req, res) => {
-  const product = {
-    id: 1,
-    title: 'lenovo thinkpad',
-    price: 3000,
-  };
-  res.status(200).send(product);
+app.get(`${api}/products`, async (req, res) => {
+  const products = await Product.find();
+
+  if (!products) {
+    res.status(500).json({
+      success: false,
+    });
+  }
+  res.status(200).send(products);
 });
 
 app.post(`${api}/products`, (req, res) => {
-  const newProduct = req.body;
-  console.log(newProduct);
-  res.status(200).send(newProduct);
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock,
+  });
+  product
+    .save()
+    .then((createdProduct) => {
+      res.status(201).json(createdProduct);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+        success: false,
+      });
+    });
 });
 
 const port = 3000;
