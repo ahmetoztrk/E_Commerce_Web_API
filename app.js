aedebbb0aae13056dfda8a06b95f30f62783cb69
ahmetoztrk <ahmetoztrk1709@gmail.com>
2024-04-04 22:42:00 +0300

    created server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/' , (req,res) => {
+    res.send('HELLO !')
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running http:localhost:${PORT}`)
+})
