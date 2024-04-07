1875d61f9b7dbe22cf6c5cb53516aa6e5abea16e
ahmetoztrk <ahmetoztrk1709@gmail.com>
2024-04-08 00:37:52 +0300

    updated product GET & POST methods

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { Product } = require('../models/product');
+const { Category } = require('../models/category');
 
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
-  const productList = await Product.find();
+  const productList = await Product.find().select("name description -_id").populate("category");
 
   if (!productList) {
     res.status(500).json({ success: false });
@@ -12,23 +13,38 @@ router.get(`/`, async (req, res) => {
   res.send(productList);
 });
 
-router.post(`/`, (req, res) => {
+router.get(`/:id`, async (req, res) => {
+  const product = await Product.findById(req.params.id).populate('category');
+
+  if (!product) {
+    res.status(500).json({ success: false });
+  }
+  res.send(product);
+});
+
+router.post(`/`, async (req, res) => {
+  const category = await Category.findById(req.body.category);
+  if (!category) return res.status(400).send('Invalid category!');
+
   const product = new Product({
     name: req.body.name,
+    description: req.body.description,
+    richDescription: req.body.richDescription,
     image: req.body.image,
+    images: req.body.images,
+    brand: req.body.brand,
+    price: req.body.price,
+    category: req.body.category,
     countInStock: req.body.countInStock,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+    isFeatured: req.body.isFeatured,
   });
-  product
-    .save()
-    .then((createdProduct) => {
-      res.status(201).json(createdProduct);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-        success: false,
-      });
-    });
+  const savedProduct = await product.save();
+
+  if (!product) return res.status(500).send('The Product cannot be created');
+
+  res.send(savedProduct);
 });
 
 module.exports = router;
